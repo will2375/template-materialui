e78b6de7b91cd942d68707480d571c3c556beda5
willi <wil.liam_benicio@hotmail.com>
2024-06-01 12:15:39 -0300

    update

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+//import MenuIcon from '@mui/icons-material/Menu';
+ 
+function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="absolute">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color= #fff
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              {/*  
+              <MenuIcon />
+              */}
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Gerenciamento de Projetos
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+}
+ 
+export default Header;
